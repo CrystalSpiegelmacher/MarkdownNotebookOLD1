2898b07350abbd6326d80ad3e6babdeed7742240
Crystal Spiegelmacher <crystalspiegelmacher@gmail.com>
2018-07-14 21:11:25 -0700

    stuff

--- API/tools/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Access-Control-Allow-Origin: ' . $origin);
-
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
-header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, trinkApiMode');
-
-
